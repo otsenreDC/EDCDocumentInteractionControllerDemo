2af75aa8a914f6c9bcc6ef58e6be3f35f9f3481c
Ernesto De los Santos Cordero <ernestodlsc@gmail.com>
2015-08-24 11:01:45 -0400

    Initial Commit

---
+++ Documents/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Documents
+//
+//  Created by Ernesto De los Santos Cordero on 8/24/15.
+//  Copyright (c) 2015 Bananalabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Documents/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Documents
+//
+//  Created by Ernesto De los Santos Cordero on 8/24/15.
+//  Copyright (c) 2015 Bananalabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
